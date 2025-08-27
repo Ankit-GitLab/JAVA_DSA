3d9a44e5add3cd92f663ac2aae484b49f592bda9
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-08-28 00:17:28 +0530

    Source Code

--- src/Conditional_loops/fibo.java
+++ src/Conditional_loops/fibo.java
@@ -5,16 +5,16 @@ public class fibo {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int a = 0;
-        int b = 1;
+        int i = 1;
         
         int count = 2;
         
         while(count <= n){
-            int temp = b;
-            b = b + a;
+            int temp = i;
+            i = i + a;
             a = temp;
             count++;
         }
-        System.out.println(b);
+        System.out.println(i);
     }
 }
