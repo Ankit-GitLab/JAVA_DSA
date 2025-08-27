138982c004f4dd847b6dfad1e9c418a06667a7a6
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-08-27 19:26:24 +0530

    Source Code

--- src/Conditional_loops/ForLoop.java
+++ src/Conditional_loops/ForLoop.java
@@ -1,7 +1,7 @@
 package Conditional_loops;
 
 import java.util.Scanner;
-public class Loops {
+public class ForLoop {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number : ");
