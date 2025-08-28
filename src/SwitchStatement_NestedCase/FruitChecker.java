70a1a62c133d4817e44cb3c359f741b9ae611dc7
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-08-28 18:53:25 +0530

    Source Code:

--- src/SwitchStatement_NestedCase/FruitChecker.java
+++ src/SwitchStatement_NestedCase/FruitChecker.java
@@ -8,11 +8,21 @@ public class FruitChecker {
         String fruit = sc.next();
 
         switch (fruit) {
-            case "Mango" -> System.out.println("King of fruit");
-            case "Apple" -> System.out.println("A sweet red fruit ");
-            case "Orange" -> System.out.println("Round fruit");
-            case "Grapes" -> System.out.println("Small fruit");
-            default -> System.out.println("Please enter a valid fruit");
+            case "Mango":
+                System.out.println("King of fruit");
+                break;
+            case "Apple":
+                System.out.println("A sweet red fruit ");
+                break;
+            case "Orange":
+                System.out.println("Round fruit");
+                break;
+            case "Grapes":
+                System.out.println("Small fruit");
+                break;
+            default:
+                System.out.println("Please enter a valid fruit");
+                break;
         }
         sc.close();
     }
