42891a47131fe9267c23e137ef187435f8067df9
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-08-28 22:30:07 +0530

    Source cODE

--- src/Methods/SumOfTwoNumber.java
+++ src/Methods/SumOfTwoNumber.java
@@ -2,17 +2,17 @@ package Methods;
 import java.security.PublicKey;
 import java.util.Scanner;
 public class SumOfTwoNumber {
-    public static int Add(int num1, int num2){
-        return num1 + num2;
-    }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter first number : ");
-        int num1 = sc.nextInt();
-        System.out.print("Enter Second number : ");
-        int num2 = sc.nextInt();
-
-        int Ans = Add(num1, num2);
-        System.out.println(num1+" + "+num2+" = "+Ans);
+        sum();
     }
+    static void sum(){
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Enter first number : ");
+       int num1 = sc.nextInt();
+        System.out.print("Enter Second number : ");
+         int num2 = sc.nextInt();
+         int Ans = num1 + num2;
+         System.out.println(num1+" + "+num2+" = "+Ans);
+    }
+
 }
