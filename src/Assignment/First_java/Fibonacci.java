2fb2cb1d25f8a16885c58df4c98278bcfbd94551
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-08-29 23:34:09 +0530

    Source Code

---
+++ src/Assignment/First_java/Fibonacci.java
@@ -0,0 +1,21 @@
+//To calculate Fibonacci Series up to n numbers.
+package Assignment.First_java;
+import java.awt.*;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int num = sc.nextInt();
+        int first = 0, second = 1, next;
+
+        for(int i=1; i<=num; i++){
+            System.out.print(first+" ");
+            next = first + second;
+            first = second;
+            second = next;
+        }
+        sc.close();
+    }
+}
