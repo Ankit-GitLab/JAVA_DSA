5a8163d37aa21beab8edcd1232f78c8c84d5dbbf
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-08-29 21:50:21 +0530

    Source Code

---
+++ src/Assignment/First_java/EvenOdd.java
@@ -0,0 +1,16 @@
+//Write a program to print whether a number is even or odd, also take input from the user.
+package Assignment.First_java;
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int num = sc.nextInt();
+        if(num%2==0){
+            System.out.println("Even");
+        }else
+            System.out.println("Odd");
+        sc.close();
+    }
+}
