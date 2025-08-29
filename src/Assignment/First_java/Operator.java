97933003ce0bd8eb13f83ffbf8e4e0488b95e035
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-08-29 22:26:25 +0530

    Source Code

---
+++ src/Assignment/First_java/Operator.java
@@ -0,0 +1,46 @@
+//Take in two numbers and an operator (+, -, *, /) and calculate the value. (Use if conditions)
+package Assignment.First_java;
+import java.util.Scanner;
+
+public class Operator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        float num1 = sc.nextFloat();
+
+        System.out.print("Enter second number: ");
+        float num2 = sc.nextFloat();
+
+        System.out.print("Enter operator (+, -, *, /): ");
+        char operator = sc.next().charAt(0);
+
+        float result = 0;
+
+        if (operator == '+') {
+            result = num1 + num2;
+            System.out.println("Addition: " + result);
+        }
+        else if (operator == '-') {
+            result = num1 - num2;
+            System.out.println("Subtraction: " + result);
+        }
+        else if (operator == '*') {
+            result = num1 * num2;
+            System.out.println("Multiplication: " + result);
+        }
+        else if (operator == '/') {
+            if (num2 != 0) {
+                result = num1 / num2;
+                System.out.println("Division: " + result);
+            } else {
+                System.out.println("Error: Cannot divide by zero!");
+            }
+        }
+        else {
+            System.out.println("Invalid operator!");
+        }
+
+        sc.close();
+    }
+}
