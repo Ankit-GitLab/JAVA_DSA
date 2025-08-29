85abc3bd5318f0aa0a18f34cc8d8979edad853a7
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-08-29 22:14:48 +0530

    Assignment Question

--- src/Assignment/First_java/Greeting.java
+++ src/Assignment/First_java/Greeting.java
@@ -5,7 +5,7 @@ public class Greeting {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Please your name : ");
-        String name = sc.next();
+        String name = sc.nextLine();
         System.out.println("Welcome "+name+" to my java program!");
         sc.close();
     }
