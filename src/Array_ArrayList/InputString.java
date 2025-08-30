c654e582a53cea5cdf5aaac2c9109fb42851f290
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-08-30 12:46:32 +0530

    Source Code

--- src/Array_ArrayList/InputString.java
+++ src/Array_ArrayList/InputString.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 public class InputString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] str = new String[4];
-        for (int i = 0; i < str.length; i++) {
+        String[] str = new String[4];        for (int i = 0; i < str.length; i++) {
             str[i] = sc.nextLine();
         }
+        System.out.println(Arrays.toString(str));
+        str[0] ="Ankit kumar";
         System.out.println(Arrays.toString(str));
     }
 }
