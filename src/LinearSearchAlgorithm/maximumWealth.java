e217a76085f8592bfc6a1337e64c3df0f8882c8c
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-09-01 00:01:08 +0530

    Source Code

---
+++ src/LinearSearchAlgorithm/maximumWealth.java
@@ -0,0 +1,15 @@
+package LinearSearchAlgorithm;
+// 1672 ---> https://leetcode.com/problems/richest-customer-wealth/description/
+public class maximumWealth {
+    public static void main(String[] args) {
+
+    }
+    public int maximumWealth(int[][] accounts){
+        // person = row,   account = column
+        for(int person = 0; person < accounts.length; person++){
+            for(int account = 0; account < accounts[person].length; account++){
+
+            }
+        }
+    }
+}
