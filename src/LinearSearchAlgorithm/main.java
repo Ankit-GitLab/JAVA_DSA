a27bceca0ce4ec894a2de254d65f766548676cc4
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-08-31 11:48:49 +0530

    sourch

--- src/LinearSearchAlgorithm/main.java
+++ src/LinearSearchAlgorithm/main.java
@@ -3,10 +3,33 @@ package LinearSearchAlgorithm;
 public class main {
     public static void main(String[] args) {
         int[] num = {23, 45, 46, 12, 78, 98, 56};
-        int target = 22;
+        int target = 12;
         int ans = linearSearch(num, target);
         System.out.println(ans);
+        boolean ans2 = findElement(num, target);
+        System.out.println(ans2);
     }
+
+    // true or false
+    static boolean findElement(int[] arr, int target) {
+        if (arr.length == 0) {
+            return false;
+        }
+
+        //run a for loop
+        for (int index = 0; index < arr.length; index++) {
+            //check for element at every index if it is = target
+            int element = arr[index];
+            if (element == target) {
+                return true;
+            }
+
+        }
+        // this line will execute if none of the return statement above have executed
+        //hence the target not found
+        return false;
+    }
+
     static int linearSearch(int[] arr, int target){
         if (arr.length == 0){
             return -1;
