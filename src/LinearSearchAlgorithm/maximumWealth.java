559c2acce10d6219b3ae598db97512919660ddd0
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-09-01 00:10:38 +0530

    Source Code

--- src/LinearSearchAlgorithm/maximumWealth.java
+++ src/LinearSearchAlgorithm/maximumWealth.java
@@ -6,10 +6,18 @@ public class maximumWealth {
     }
     public int maximumWealth(int[][] accounts){
         // person = row,   account = column
-        for(int person = 0; person < accounts.length; person++){
-            for(int account = 0; account < accounts[person].length; account++){
-
+        int ans = Integer.MIN_VALUE;
+        for (int[] ints : accounts) {
+            int sum = 0;
+            for (int anInt : ints) {
+                sum += anInt;
+            }
+            //now we have sum of account of person
+            //check with overall answer
+            if (sum > ans) {
+                ans = sum;
             }
         }
+        return ans;
     }
 }
