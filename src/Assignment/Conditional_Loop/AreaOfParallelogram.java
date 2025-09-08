da5de73ed1364f988a94cdeb9057116fa264f626
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-09-08 12:17:36 +0530

    Source Code AssignMent

---
+++ src/Assignment/Conditional_Loop/AreaOfParallelogram.java
@@ -0,0 +1,14 @@
+package Assignment.Conditional_Loop;
+import java.util.Scanner;
+public class AreaOfParallelogram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Base: ");
+        double base = sc.nextDouble();
+        System.out.print("Enter height: ");
+        double height = sc.nextDouble();
+        double Area = base * height;
+        System.out.println("Your Area is: "+Area);
+        sc.close();
+    }
+}
