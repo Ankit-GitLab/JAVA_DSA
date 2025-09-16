666dcf44f36306ecd0a1f86e896302637c7167eb
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-09-16 11:33:39 +0530

    Source Code

---
+++ src/BinarySearch/PeakIndex.java
@@ -0,0 +1,21 @@
+package BinarySearch;
+
+public class PeakIndex {
+
+    public static int peakIndexInMountainArray(int[] arr){
+        int  lo = 0, hi = arr.length;
+        while(lo < hi){
+            int mid = lo + (hi - lo) / 2;
+            if(arr[mid] < arr[mid+1]){
+                lo = mid + 1;
+            }else{
+                hi = mid;
+            }
+        }
+        return lo;
+    }
+    public static void main(String[] args) {
+        int[] arr = {0, 2, 5, 7, 6, 3, 1};
+        System.out.println("Peak Index: " + peakIndexInMountainArray(arr));
+    }
+}
