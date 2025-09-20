a9626b9872ddb7e0a5593223d3bf7fdba5b39591
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-09-20 18:02:51 +0530

    Source Code

--- src/BinarySearch/First_Occurrence.java
+++ src/BinarySearch/First_Occurrence.java
@@ -9,12 +9,9 @@ public class First_Occurrence {
         int lo = 0, hi = arr.length - 1, idx = -1;
         while (lo <= hi) {
             int mid = (lo + hi) / 2;
-
-            if (arr[mid] > target) {
-                hi = mid - 1;
-            } else if (arr[mid] < target) {
-                lo = mid + 1;
-            } else {
+            if (arr[mid] > target) hi = mid - 1;
+            else if (arr[mid] < target) lo = mid + 1;
+            else{
                 idx = mid;
                 hi = mid - 1;
             }
