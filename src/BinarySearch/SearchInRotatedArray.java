d6225c875ea55b44f1950786e3b15bfb8f67ae39
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-09-21 14:04:39 +0530

    Source Code

--- src/BinarySearch/SearchInRotatedArray.java
+++ src/BinarySearch/SearchInRotatedArray.java
@@ -1,41 +1,28 @@
 package BinarySearch;
 
 public class SearchInRotatedArray {
-    public static int search(int[] arr, int key) {
-        int n = arr.length;
-        int lo = 0, hi = n - 1;
-
+    public static int search(int[] arr, int tar) {
+        int lo = 0, hi = arr.length - 1;
         while (lo <= hi) {
             int mid = lo + (hi - lo) / 2;
+            if (arr[mid] == tar) return mid;
 
-            // If element is found
-            if (arr[mid] == key) {
-                return mid;
-            }
-
-            // Check if left half is sorted
-            if (arr[lo] <= arr[mid]) {
-                if (key >= arr[lo] && key < arr[mid]) {
-                    hi = mid - 1; // search left side
-                } else {
-                    lo = mid + 1;  // search right side
-                }
+            else if(arr[lo] <= arr[mid]) {
+                if (tar >= arr[lo] && tar < arr[mid])  hi = mid - 1; // search left side
+                 else  lo = mid + 1;  // search right side
             }
             // Otherwise, right half must be sorted
             else {
-                if (key > arr[mid] && key <= arr[hi]) {
-                    lo = mid + 1;  // search right side
-                } else {
-                    hi = mid - 1; // search left side
-                }
+                if (tar > arr[mid] && tar <= arr[hi])  lo = mid + 1;  // search right side
+                 else  hi = mid - 1; // search left side
             }
         }
-        return -1; // element not found
+        return -1;
     }
 
     public static void main(String[] args) {
         int[] arr = {5, 6, 7, 8, 9, 10, 1, 2, 3};
-        int key = 10;
+        int key = 9;
 
         int index = search(arr, key);
         if (index != -1)
