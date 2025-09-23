8aa4cea444e612f894f2fb2e9a9b40b234bf7832
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-09-23 17:23:58 +0530

    String

---
+++ src/Strings/Plus.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class Plus {
+    public static void main(String[] args) {
+        String a = "Ankit";
+        a = a + " is a good man";
+        System.out.println(a);
+    }
+}
