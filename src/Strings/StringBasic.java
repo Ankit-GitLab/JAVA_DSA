17cc89c5b4b4698187bd1ee42cfd59d0dcf7aa78
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-09-23 08:41:01 +0530

    String

---
+++ src/Strings/StringBasic.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class StringBasic {
+    public static void main(String[] args) {
+        char[] arr = {'A','N','K','I','T'};
+        for(char ele : arr){
+            System.out.print(ele+" ");
+        }
+    }
+}
