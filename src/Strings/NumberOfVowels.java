febfddc2acaa8287f4d47936645c3fbc99bbe569
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-09-23 09:14:47 +0530

    Source Code

---
+++ src/Strings/NumberOfVowels.java
@@ -0,0 +1,14 @@
+package Strings;
+
+public class NumberOfVowels {
+    public static void main(String[] args) {
+        String str = "ankitriya";
+        int count = 0;
+        for(int i=0; i<str.length(); i++){
+            if('a'==str.charAt(i) || 'e' ==str.charAt(i) || 'i' ==str.charAt(i) || 'o' ==str.charAt(i) || 'u' ==str.charAt(i)){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
