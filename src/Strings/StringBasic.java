b44e7eec426c6f074d3f0115644b33b64c60ec62
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-09-23 09:06:58 +0530

    String

--- src/Strings/StringBasic.java
+++ src/Strings/StringBasic.java
@@ -1,10 +1,9 @@
 package Strings;
-
+import java.util.Scanner;
 public class StringBasic {
     public static void main(String[] args) {
-        char[] arr = {'A','N','K','I','T'};
-        for(char ele : arr){
-            System.out.print(ele+" ");
-        }
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(str);
     }
 }
