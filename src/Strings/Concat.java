79abea1977c0a3c91e9dfaf2aef6694f1a13d413
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-09-23 17:20:16 +0530

    String

---
+++ src/Strings/Concat.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class Concat {
+    public static void main(String[] args) {
+        String a = "AnKiT ";
+        String b = "Kumar";
+        a = a.concat(b);
+        System.out.println(a);
+    }
+}
