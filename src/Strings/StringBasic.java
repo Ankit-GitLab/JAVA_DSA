c3fda1a68975bf5379fa6c29c298d54cdeff5e8e
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-09-24 09:35:49 +0530

    String

--- src/Strings/StringBasic.java
+++ src/Strings/StringBasic.java
@@ -1,9 +1,9 @@
 package Strings;
-import java.util.Scanner;
 public class StringBasic {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
+
+        String str = "Ankit kumar";
         System.out.println(str);
+        System.out.println(str.substring(5,str.length()-1));
     }
 }
--- src/Strings/StringToCharArray.java
+++ src/Strings/StringToCharArray.java
@@ -4,5 +4,8 @@ public class StringToCharArray {
     public static void main(String[] args) {
         String s = "Ruhan";
         char[] arr = s.toCharArray();
+        for(char ch : arr){
+            System.out.println(ch);
+        }
     }
 }
