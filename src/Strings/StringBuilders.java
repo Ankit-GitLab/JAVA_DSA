59e1829cebcaa42c785515c4f9297e7ec7d04cae
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-09-25 10:41:31 +0530

    String

--- src/Strings/StringBuilders.java
+++ src/Strings/StringBuilders.java
@@ -3,8 +3,11 @@ package Strings;
 public class StringBuilders {
     public static void main(String[] args) {
         StringBuilder s = new StringBuilder("Ankit");
+        System.out.println(s.length()+" "+s.capacity());
         System.out.println(s);
         s.append("Kumar");
+        System.out.println(s);
+        s.setCharAt(1,'m');
         System.out.println(s);
     }
 }
