496c0976807e6101b6c404fab4494d2cac4ee73a
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-09-25 10:10:02 +0530

    String

---
+++ src/Strings/EqualTo.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class EqualTo {
+    public static void main(String[] args) {
+        String s1 = new String("Ankit");
+        String s2 = new String("Ankit");
+        System.out.println(s1==s2); // because this address is different
+    }
+}
