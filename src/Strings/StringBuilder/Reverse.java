0d3ee5e348e0910c177829cf909b3dcd78c367f3
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-09-25 10:50:36 +0530

    String

--- src/Strings/StringBuilder/Reverse.java
+++ src/Strings/StringBuilder/Reverse.java
@@ -4,6 +4,7 @@ public class Reverse {
     public static void main(String[] args) {
         String s = "Ankit";
         StringBuilder sb = new StringBuilder(s);
+        sb.append("Kumar");
         System.out.println(sb);
     }
 }
