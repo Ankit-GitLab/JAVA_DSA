6fbbd465e53a82841ffed9931838be301ee841ee
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-09-25 10:49:42 +0530

    String

---
+++ src/Strings/StringBuilder/Reverse.java
@@ -0,0 +1,9 @@
+package Strings.StringBuilder;
+
+public class Reverse {
+    public static void main(String[] args) {
+        String s = "Ankit";
+        StringBuilder sb = new StringBuilder(s);
+        System.out.println(sb);
+    }
+}
