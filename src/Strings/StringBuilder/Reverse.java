a42c3b369d2ec3cdd87d13b49845aca6369acc70
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-09-26 22:29:45 +0530

    String

--- src/Strings/StringBuilder/Reverse.java
+++ src/Strings/StringBuilder/Reverse.java
@@ -6,14 +6,7 @@ public class Reverse {
         StringBuilder sb = new StringBuilder(s);
         System.out.println(sb);
         int i=0, j = sb.length()-1;
-        while(i<=j){
-            char temp1 = sb.charAt(i);
-            char temp2 = sb.charAt(j);
-            sb.setCharAt(i,temp2);
-            sb.setCharAt(j,temp1);
-            i++;
-            j--;
-        }
+
         System.out.println(sb);
 
         sb.reverse();
