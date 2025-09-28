5916ca319d1cd42de63b70a6649d1ca61d84c2ce
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-09-28 10:43:29 +0530

    OOP

---
+++ src/OPPS/UserDefinedDataType.java
@@ -0,0 +1,12 @@
+package OPPS;
+
+public class UserDefinedDataType {
+    public static class Student{
+        String name;
+        int rno;
+        double cgpa;
+    }
+    public static void main(String[] args) {
+        Student s = new Student();
+    }
+}
