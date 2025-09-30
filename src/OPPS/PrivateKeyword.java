9c959151c1faac5602a262a7770f4a65fc86fdcd
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-09-30 17:33:26 +0530

    OOP

--- src/OPPS/PrivateKeyword.java
+++ src/OPPS/PrivateKeyword.java
@@ -3,11 +3,14 @@ class Students{
     String name ;
     private int rno;
     double cgpa;
-    private void print() {
+    void print() { // getter
         System.out.println(name+" "+cgpa+" "+rno);
     }
-    public void p(){
-        print();
+    int  getRno(){ // getter
+        return rno;
+    }
+    void setRno(int x){ // setter
+        rno = x;
     }
 }
 public class PrivateKeyword {
@@ -17,9 +20,12 @@ public class PrivateKeyword {
         System.out.println(s1.name);
         s1.cgpa = 7.3;
         s1.name = "Ankit";
-        // s1.rno = 12;
-        s1.p();
+
+        s1.print();
         Students s2 = new Students();
-        s2.p();
+        s2.print();
+        System.out.println(s1.getRno());
+        s1.setRno(246003);
+        System.out.println(s1.getRno());
     }
 }
