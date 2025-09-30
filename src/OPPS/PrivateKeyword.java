af3e81ddf30569d7e5ce8cde82ba20878f89da4e
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-09-30 11:57:22 +0530

    OPP

---
+++ src/OPPS/PrivateKeyword.java
@@ -0,0 +1,11 @@
+package OPPS;
+
+public class PrivateKeyword {
+    public static class Students{
+        String name ;
+        int rno;
+    }
+    public static void main(String[] args) {
+
+    }
+}
