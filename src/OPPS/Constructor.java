b899fe2bdf734156caee0e8a25c026b0049e6d5e
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-10-01 23:47:08 +0530

    OOP

--- src/OPPS/Constructor.java
+++ src/OPPS/Constructor.java
@@ -17,12 +17,21 @@ public class Constructor {
             System.out.println(seats+" "+name+" "+length);
         }
     }
+    public static int max(int a, int b){
+        return Math.max(a,b);
+    }
+    public static int max(int a, int b, int c){
+        return Math.max(a,Math.max(b,c));
+    }
 
     public static void main(String[] args) {
-        Car c1 = new Car(5, "Kia Sonet",3.99);
-        c1.print();
-        Car c2 = new Car(4, "Lord Alto",4.59);
-        c2.print();
-        Car c3 = new Car();
+        System.out.println(max(9,4));
+        System.out.println(max(1,4, 5));
+
+//        Car c1 = new Car(5, "Kia Sonet",3.99);
+//        c1.print();
+//        Car c2 = new Car(4, "Lord Alto",4.59);
+//        c2.print();
+//        Car c3 = new Car();
     }
 }
