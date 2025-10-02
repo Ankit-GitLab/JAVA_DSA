ad562700453d552d57e51114ca218817e20d7f8d
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-10-02 18:51:17 +0530

    OOP

--- src/OPPS/RevisionOOPs.java
+++ src/OPPS/RevisionOOPs.java
@@ -2,20 +2,15 @@ package OPPS;
 
 public class RevisionOOPs {
     private static class Pokemon{
-        int hp;
-        int speed;
-        int attack;
-        int defense;
+        int power;
         String type;
-        String name;
+        Pokemon(String type, int power){
+            this.power = power;
+            this.type = type;
+        }
     }
     public static void main(String[] args) {
-        Pokemon p1 = new Pokemon();
-        p1.name = "Pikachu"; p1.type = "Electric"; p1.attack = 70;
-        Pokemon p2 = new Pokemon() ;
-        p2.name = "Charizard"; p2.type = "fire"; p1.attack = 110;
-        Pokemon p3 = p1; // p3 is shallow copy of p1 (can control)
-        p3.type = "Water";
-        System.out.println(p1.type);
+        Pokemon p1 = new Pokemon("Electric",70);
+
     }
 }
