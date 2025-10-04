ad9697034a59596ac568ec566ae7c1212513cf77
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-10-04 18:00:02 +0530

    OOP

--- src/OPPS/FinalAndStatic.java
+++ src/OPPS/FinalAndStatic.java
@@ -1,14 +1,25 @@
 package OPPS;
 
 class Cricketer{
-    static String country = "India";
+    static String country;
     int runs;
     String name;
     double avg;
+    void print(){
+        System.out.println(runs+" "+name+" "+avg);
+    }
+    static void greet(){
+        System.out.println("I only believe in jassi bhai");
+    }
 }
 public class FinalAndStatic {
     public static void main(String[] args) {
-        Cricketer c1 = new Cricketer() ;
-        System.out.println(c1.country);
+        Cricketer.greet();
+//        Cricketer c1 = new Cricketer() ;
+//        System.out.println(c1.country);
+//        Cricketer c2 = new Cricketer() ;
+//        System.out.println(c2.country);
+//        c1.country = "india";
+//        System.out.println(c2.country);
     }
 }
