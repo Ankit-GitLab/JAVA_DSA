a49bbecd7c4d6cb8365288a983ac6de73fa7bf2e
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-10-05 15:05:41 +0530

    OOP

--- src/OPPS/FractionClass.java
+++ src/OPPS/FractionClass.java
@@ -18,8 +18,12 @@ class Fraction{
         den = den * f.den;
     }
     void multiple(Fraction f){
-        num *= f.num;
-        den *= f.den;
+        this.num *= f.num;
+        this.den *= f.den;
+    }
+    void divide(Fraction f){
+        num = den*f.num;
+        den = num*f.den;
     }
 }
 public class FractionClass {
@@ -31,5 +35,8 @@ public class FractionClass {
 //        f1.print();
         f1.multiple(f2);
         f1.print();
+        Fraction f3 = new Fraction(50, 100);
+        f3.divide(f3);
+        f3.print();
     }
 }
