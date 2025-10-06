c2e7ef93aa13858631e2adef43c261c82376cb57
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-10-06 10:40:49 +0530

    Rec

--- src/Recursion/NumberExampleRecursion.java
+++ src/Recursion/NumberExampleRecursion.java
@@ -7,7 +7,13 @@ public class NumberExampleRecursion {
         print(1);
     }
     static void print(int n){
-        if(n==5) return;
+        //base condition
+        if(n==5){
+            System.out.println(n);
+            return;
+        }
+        //recursive call
+        // if you are calling a function again and again, you can treat it as a separate in the stack
         System.out.print(n+" ");
         print(n + 1);
 
