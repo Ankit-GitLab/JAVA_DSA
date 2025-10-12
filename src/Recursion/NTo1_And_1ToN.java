b9e7ccfce5cd6e174aa4208a261420ad728d8b84
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-10-12 10:19:18 +0530

    Rec

---
+++ src/Recursion/NTo1_And_1ToN.java
@@ -0,0 +1,18 @@
+package Recursion;
+import java.util.Scanner;
+public class NTo1_And_1ToN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int num = sc.nextInt();
+        funBoth(num);
+    }
+    static void funBoth(int n){
+        if(n==0) {
+            return;
+        }
+        System.out.print(n+" ");
+        funBoth(n-1);
+        System.out.print(n+" ");
+    }
+}
