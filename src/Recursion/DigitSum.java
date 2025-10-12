89dc2c30383702dec8323086783be5a6bff497a8
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-10-12 15:20:04 +0530

    REC

---
+++ src/Recursion/DigitSum.java
@@ -0,0 +1,12 @@
+package Recursion;
+
+public class DigitSum {
+    public static void main(String[] args) {
+        int ans = sum(234345);
+        System.out.println("The sum of digit is: "+ans);
+    }
+    static int sum(int n) {
+        if (n == 0) return 0;
+        return (n % 10) + sum(n / 10);
+    }
+}
