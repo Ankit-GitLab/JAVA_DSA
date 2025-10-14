7c5e98f45d5979b08f994dea0ad8c3a32f626663
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-10-14 23:40:30 +0530

    Rec

--- src/Recursion/ReverseNumber.java
+++ src/Recursion/ReverseNumber.java
@@ -15,7 +15,11 @@ public class ReverseNumber {
     }
 
     static int helper(int n, int digits) {
-
+         if (n%10 == n){
+            return n;
+        }
+         int rem = n % 10;
+         return rem * (int)Math.pow(10, digits-1) + helper(n/10, digits - 1);
     }
 
     static int sum = 0;
