d44e595a267a17b76b23802985126b2762ceb7cc
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-10-14 23:30:11 +0530

    REC

--- src/Recursion/ReverseNumber.java
+++ src/Recursion/ReverseNumber.java
@@ -5,7 +5,15 @@ public class ReverseNumber {
         int n = 123456;
         reverse(n);
         System.out.println(sum);
+        System.out.println(rev2(n));
     }
+    static int rev2(int n){
+        // sometimes you might need some additional variables in the argument
+        // in that case, make another function
+        int digits = (int) (Math.log10(n)) + 1;
+        return digits;
+    }
+
     static int sum = 0;
     static void reverse(int n) {
         int lastDigit = (n%10);
