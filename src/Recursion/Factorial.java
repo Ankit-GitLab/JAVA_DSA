c7c1be2f09960be96de076157dce2edbc37441c7
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-10-15 22:11:34 +0530

    REC

--- src/Recursion/Factorial.java
+++ src/Recursion/Factorial.java
@@ -8,6 +8,9 @@ public class Factorial {
         int num = sc.nextInt();
         System.out.println(fact(num));
     }
-
-
+    private static int fact(int n) {
+        // base condition
+        if(n==0 || n==1) return n;
+        return n*fact(n-1);
+    }
 }
