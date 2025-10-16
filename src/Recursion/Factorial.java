d4549abcce6337eb5a2104a38bd901aa8e7a7781
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-10-16 23:14:10 +0530

    Rec

--- src/Recursion/Factorial.java
+++ src/Recursion/Factorial.java
@@ -3,7 +3,9 @@ package Recursion;
 import java.util.Scanner;
 public class Factorial {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int num = sc.nextInt();
         System.out.println(fact(num));
     }
     private static int fact(int n) {
