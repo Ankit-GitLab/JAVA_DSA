0625ebf71b7fd8b7defd9a46312a3599aa23576f
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-10-17 21:04:00 +0530

    Rec

--- src/Recursion/Array/ArrayIsSortedOrNot.java
+++ src/Recursion/Array/ArrayIsSortedOrNot.java
@@ -2,14 +2,16 @@ package Recursion.Array;
 
 public class ArrayIsSortedOrNot {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 4, 3, 5, 3};
-        System.out.println(sort(arr, 0, 1));
+        int[] arr = {1, 2, 3, 6, 4, 5};
+        System.out.println(sort(arr, 0));
     }
 
-    static boolean sort(int[] arr, int i, int j) {
-        int n = arr.length;
-        if(){
-
+    static boolean sort(int[] arr, int index) {
+        // Base case: if j reaches the last index, array is sorted
+        if (index == arr.length - 1) {
+            return true;
         }
+        // check the array is sorted or not
+        return arr[index] < arr[index+1] && sort(arr, index+1);
     }
 }
