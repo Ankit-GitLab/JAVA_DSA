18200531c6e4dd0dee43e83dbebe5c61eb928320
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-10-17 18:19:00 +0530

    Rec

--- src/Recursion/CountZero.java
+++ src/Recursion/CountZero.java
@@ -3,17 +3,20 @@ package Recursion;
 public class CountZero {
     public static void main(String[] args) {
         int num = 102034;
-        System.out.println(count(num));
+        int digit = 0;
+        System.out.println(count(num, digit));
     }
 
-    static int count(int num) {
-        int count = 0;
+    static int count(int num , int digit) {
         int rem = num % 10;
-
-        if(rem == 10){
-            count++;
-            return count(num/10);
+        if (num == 0) {
+            return digit;
         }
-        return count;
+        if(rem == 0) {
+                return count(num / 10, digit+1);
+        }else{
+                return count(num / 10, digit);
+        }
+//        return digit;
     }
 }
