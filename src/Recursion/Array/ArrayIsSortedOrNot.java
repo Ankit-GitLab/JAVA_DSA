3ce13faa30e63510c8504f2f813b7ad1db20a2a6
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-10-17 20:44:34 +0530

    Rec

---
+++ src/Recursion/Array/ArrayIsSortedOrNot.java
@@ -0,0 +1,15 @@
+package Recursion.Array;
+
+public class ArrayIsSortedOrNot {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 3, 5, 3};
+        System.out.println(sort(arr, 0, 1));
+    }
+
+    static boolean sort(int[] arr, int i, int j) {
+        int n = arr.length;
+        if(){
+
+        }
+    }
+}
