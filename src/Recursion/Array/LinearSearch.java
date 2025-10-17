ad863709f876d969ddad2cf7d67cd9458d05b6ec
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-10-17 23:39:56 +0530

    Rec

--- src/Recursion/Array/LinearSearch.java
+++ src/Recursion/Array/LinearSearch.java
@@ -4,13 +4,15 @@ import java.util.ArrayList;
 
 public class LinearSearch {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7,9};
+        int[] arr = {1,2,3,4,5,4,7,9};
         int target = 5;
         System.out.println("index number is : "+findIndex(arr, target, 0));
         System.out.println("index number is : "+find(arr, target, 0));
         System.out.println("index number is : "+findLastIndex(arr, target, arr.length-1));
-        findAllIndex(arr,4,0);
-        System.out.println(list);
+       // findAllIndex(arr,4,0);
+       // System.out.println(list);
+        int[] arr1;
+        System.out.println(findAllIndex(arr,4,0, new ArrayList<>()));
 
     }
     // print index
@@ -51,5 +53,15 @@ public class LinearSearch {
             list.add(index);
         }
         findAllIndex(arr, target, index+1);
+    }
+
+    static ArrayList<Integer> findAllIndex(int[] arr, int target, int index, ArrayList<Integer> list) {
+        if(index == arr.length){
+            return list;
+        }
+        if(arr[index] == target){
+            list.add(index);
+        }
+        return findAllIndex(arr, target, index+1, list);
     }
 }
