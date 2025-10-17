3af6c2da217ed8052dee54c8620bce1256857a33
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-10-17 21:57:31 +0530

    Rec

--- src/Recursion/Array/LinearSearch.java
+++ src/Recursion/Array/LinearSearch.java
@@ -4,17 +4,37 @@ public class LinearSearch {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7,9};
         int target = 5;
-        System.out.println("index number is : "+search(arr, target, 0));
+        System.out.println("index number is : "+findIndex(arr, target, 0));
+        System.out.println("index number is : "+find(arr, target, 0));
+        System.out.println("index number is : "+findLastIndex(arr, target, arr.length-1));
     }
-
-    static int search(int[] arr, int target, int index) {
+    // print index
+    static int findIndex(int[] arr, int target, int index) {
         if(index == arr.length-1){
             return -1;
         }
         if(arr[index] != target){
-            return search(arr, target, index+1);
+            return findIndex(arr, target, index+1);
         }else{
             return index;
         }
+    }
+
+    static int findLastIndex(int[] arr, int target, int index) {
+        if(index == -1){
+            return -1;
+        }
+        if(arr[index] != target){
+            return findLastIndex(arr, target, index-1);
+        }else{
+            return index;
+        }
+    }
+
+    // print true or false
+    static boolean find(int[] arr, int target, int index){
+        if(index == arr.length-1) return false;
+
+        return arr[index] == target || find(arr, target, index+1);
     }
 }
