53fb4f3414a149fa275053060ed82e261180577d
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-10-19 02:15:44 +0530

    Sort

--- src/Recursion/Sort/Bubble/BubbleSortByRec.java
+++ src/Recursion/Sort/Bubble/BubbleSortByRec.java
@@ -2,7 +2,7 @@ package Recursion.Sort.Bubble;
 
 import java.util.Arrays;
 
-public class Triangle {
+public class BubbleSortByRec {
     public static void main(String[] args) {
         int[] arr = {4, 3, 2, 1};
         bubble(arr, arr.length-1, 0);
