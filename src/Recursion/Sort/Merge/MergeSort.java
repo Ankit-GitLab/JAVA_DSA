7908fb8395afebed73e1a7844431cf7bd33ad114
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-10-19 12:24:25 +0530

    rec

--- src/Recursion/Sort/Merge/MergeSort.java
+++ src/Recursion/Sort/Merge/MergeSort.java
@@ -14,6 +14,29 @@ public class MergeSort {
 
         int mid = arr.length / 2;
         int[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
-        int[] right = mergeSort(Arrays.copyOfRange(arr, mid, arr.length-1));
+        int[] right = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+
+        return merge(left, right);
+    }
+
+    static int[] merge(int[] first, int[] second) {
+        int[] mix = new int[first.length + second.length];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+         while(i < first.length && j < second.length){
+             if(first[i] < second[j]){
+                 mix[k] = first[j];
+                 i++;
+             }else{
+                 mix[k] = second[j];
+                 j++;
+             }
+             k++;
+         }
+
+         //  it may be possible that one of the arrays is not completed
     }
 }
