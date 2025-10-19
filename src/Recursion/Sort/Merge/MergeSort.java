62f9f38ed77a684cc9d377995c0995e93da3fd6b
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-10-19 12:11:13 +0530

    rec

---
+++ src/Recursion/Sort/Merge/MergeSort.java
@@ -0,0 +1,19 @@
+package Recursion.Sort.Merge;
+
+import java.util.Arrays;
+
+public class MergeSort {
+    public static void main(String[] args) {
+
+    }
+
+    static int[] mergeSort(int[] arr){
+        if(arr.length == 1){
+            return arr;
+        }
+
+        int mid = arr.length / 2;
+        int[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = mergeSort(Arrays.copyOfRange(arr, mid, arr.length-1));
+    }
+}
