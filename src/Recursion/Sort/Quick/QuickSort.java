2a073ec76fc9e797e036c8d37d1e772c96c999f6
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-10-20 16:19:49 +0530

    sort

--- src/Recursion/Sort/Quick/QuickSort.java
+++ src/Recursion/Sort/Quick/QuickSort.java
@@ -4,5 +4,29 @@ public class QuickSort {
     public static void main(String[] args) {
 
     }
-    static void sort(int[] nums, )
+    static void sort(int[] nums, int low, int hi){
+        if(low >= hi){
+            return;
+        }
+
+        int s = low;
+        int e = hi;
+        int mid = s+(e-s)/2;
+        int pivot = nums[mid];
+
+        while(s <= e){
+
+            // slso
+            while(nums[s] < pivot){
+                s++;
+            }
+            while(nums[e] > pivot){
+                e--;
+            }
+
+            if(s <= e){
+
+            }
+        }
+    }
 }
