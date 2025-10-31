2cc585c1bfe1c5ba225fb31bec82a5d0abe3d0ca
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-10-31 16:08:01 +0530

    Rec

--- src/Recursion/Sort/Merge/MergeSort.java
+++ src/Recursion/Sort/Merge/MergeSort.java
@@ -54,6 +54,10 @@ public class MergeSort {
 
         // Copy remaining elements from second array (if any)
         while (j < second.length ){
+            mix[k] = second[j];
+            j++;
+            k++;
+        }
 
         return mix;
     }
