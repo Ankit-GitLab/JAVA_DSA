8d07110a5419631dd911393a5faa344b07436cf7
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-11-08 20:30:26 +0530

    REC

--- src/Project/resource/ToDoListGui.java
+++ src/Project/resource/ToDoListGui.java
@@ -6,5 +6,6 @@ public class ToDoListGui extends JFrame  {
     public ToDoListGui (){
         super(" To Do List Application ");
         setDefaultCloseOperation(EXIT_ON_CLOSE );
+        setPreferredSize(CommonConstants.GUI_SIZE);
     }
 }
