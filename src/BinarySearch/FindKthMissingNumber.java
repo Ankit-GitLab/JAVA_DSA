18361185c630a6aee12c28730d8356b41c11c136
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-11-09 16:35:01 +0530

    Rec

--- src/BinarySearch/FindKthMissingNumber.java
+++ src/BinarySearch/FindKthMissingNumber.java
@@ -7,5 +7,10 @@ public class FindKthMissingNumber {
         return hi+ 1 + k;
     }
 
-
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 4, 7, 11};
+        int k = 6;
+        int result = findKthPositive(arr, k);
+        System.out.println("The " + k + "th missing positive number is: " + result);
+    }
 }
