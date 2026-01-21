274bb1c81ac86756c01531c5d92895923d94e6b7
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-01-21 21:44:23 +0530

    Day 2

---
+++ src/OPPs/src/ATM.java
@@ -0,0 +1,17 @@
+public class ATM {
+    private String accountHolderName;
+    private double balance;
+
+    ATM(String name, double bal){
+        this.accountHolderName = name;
+        this.balance = bal;
+    }
+
+    public static void main(String[] args) {
+        ATM atm = new ATM("Ankit",10000);
+        atm.showBal();
+    }
+    void showBal(){
+        System.out.println("Your balance is : "+balance);
+    }
+}
---
+++ src/OPPs/src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        ATM atm = new ATM("Moto",5000);
+        atm.showBal();
+//        atm.balance = 10;
+        atm.showBal();
+}
+}
