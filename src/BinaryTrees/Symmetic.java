f394591832f75fddfbb2d8c1842cc81ce8966993
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-03-09 23:02:36 +0530

    symmetic tree

---
+++ src/BinaryTrees/Symmetic.java
@@ -0,0 +1,26 @@
+package BinaryTrees;
+
+public class Symmetic {
+    public boolean isSymmetric(Node root) {
+        mirror(root.left);
+        return isIdentical(root.left, root.right);
+
+    }
+    public boolean isIdentical(Node p, Node q) {
+        if(p==null && q==null) return true;
+        if(p==null || q==null) return false;
+        if(p.val != q.val) return false;
+
+        return isIdentical(p.left,q.left) && isIdentical(p.right,q.right);
+
+    }
+    void mirror(Node root) {
+        if(root==null) return;
+        Node temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        mirror(root.left);
+        mirror(root.right);
+
+    }
+}
