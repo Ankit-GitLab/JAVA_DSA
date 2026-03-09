904708c45b646ca5d307b974cb9cf6569e31fe83
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-03-09 23:25:06 +0530

    path sum

---
+++ src/BinaryTrees/PathSum.java
@@ -0,0 +1,15 @@
+package BinaryTrees;
+
+
+public class PathSum {
+    public boolean hasPathSum(Node root, int targetSum) {
+
+        if(root==null) return false;
+        if(root.left==null && root.right==null){
+            if(targetSum==root.val) return true;
+            else return false;
+        }
+        return hasPathSum(root.left,targetSum-root.val) || hasPathSum(root.right,targetSum-root.val);
+
+    }
+}
