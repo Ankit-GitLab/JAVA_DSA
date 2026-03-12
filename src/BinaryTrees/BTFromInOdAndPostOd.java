b4183d1973538bfacd6cf144234ddc913ab84e0e
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-03-12 23:45:14 +0530

    binary tree from inoder and post order

---
+++ src/BinaryTrees/BTFromInOdAndPostOd.java
@@ -0,0 +1,30 @@
+package BinaryTrees;
+
+public class BTFromInOdAndPostOd {
+    Node buildTree(int[] inorder, int[] postorder) {
+        int n = inorder.length;
+        return build(0, n-1, 0, n-1, inorder, postorder);
+    }
+
+    public static Node build(int inLo, int inHi, int postLo, int postHi, int inorder[], int postorder[]) {
+        if(inLo > inHi || postLo > postHi) return null;
+
+        int val = postorder[postHi];
+        Node root = new Node(val);
+
+        int r = inLo;
+        for(int i = inLo; i <= inHi; i++){
+            if(inorder[i] == val){
+                r = i;
+                break;
+            }
+        }
+
+        int cnt = r - inLo;
+
+        root.left = build(inLo, r-1, postLo, postLo + cnt - 1, inorder, postorder);
+        root.right = build(r+1, inHi, postLo + cnt, postHi - 1, inorder, postorder);
+
+        return root;
+    }
+}
