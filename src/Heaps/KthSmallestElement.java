b96ec7d0b8e5f9d0c54da37b9c7f93142e078320
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-03-20 15:44:18 +0530

    Kth smallest element in Heap

---
+++ src/Heaps/KthSmallestElement.java
@@ -0,0 +1,15 @@
+package Heaps;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class KthSmallestElement {
+    public int kthSmallest(int[] arr, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int ele : arr){
+            pq.add(ele);
+            if(pq.size() > k) pq.remove();
+        }
+        return pq.peek();
+    }
+}
