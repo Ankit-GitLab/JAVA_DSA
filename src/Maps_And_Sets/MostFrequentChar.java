120ade73ec2f8c632bb34bb0a6f8aaceebc61409
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-03-24 23:50:16 +0530

    Most frequent in charater

---
+++ src/Maps_And_Sets/MostFrequentChar.java
@@ -0,0 +1,17 @@
+package Maps_And_Sets;
+
+import java.util.HashMap;
+
+public class MostFrequentChar {
+    public char getMaxOccuringChar(String s) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=0; i<=s.length(); i++){
+            char ch = s.charAt(i);
+            if(map.containsKey(ch)){
+                int freq = map.get(ch);
+                map.put(ch,freq+1);
+            }
+            else map.put(ch,1);
+        }
+    }
+}
