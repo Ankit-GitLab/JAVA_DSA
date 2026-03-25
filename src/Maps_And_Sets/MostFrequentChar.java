00b292f6fc8c56f41df64701a904bbd43efe3ba7
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-03-25 18:40:47 +0530

    Array SubSet

--- src/Maps_And_Sets/MostFrequentChar.java
+++ src/Maps_And_Sets/MostFrequentChar.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class MostFrequentChar {
     public char getMaxOccuringChar(String s) {
         HashMap<Character,Integer> map = new HashMap<>();
-        for(int i=0; i<=s.length(); i++){
+        for(int i=0; i<s.length(); i++){
             char ch = s.charAt(i);
             if(map.containsKey(ch)){
                 int freq = map.get(ch);
