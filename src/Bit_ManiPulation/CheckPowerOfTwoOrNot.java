92c984120f58ea8aa4d50b77778e972302dced0e
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-03-29 23:32:29 +0530

    Power of 2 best approch

--- src/Bit_ManiPulation/CheckPowerOfTwoOrNot.java
+++ src/Bit_ManiPulation/CheckPowerOfTwoOrNot.java
@@ -1,14 +1,17 @@
 package Bit_ManiPulation;
-
 public class CheckPowerOfTwoOrNot {
-    public static boolean isPowerofTwo(int n) {
-        if(n==0) return false;
-        while(n>1){
-            if(n%2==1) return false;
-            n /= 2;
-        }
-        return true;
+        public static boolean isPowerofTwo(int n) {
+            return n > 0 && ((n&(n-1))==0);
+
     }
+//    public static boolean isPowerofTwo(int n) {
+//        if(n==0) return false;
+//        while(n>1){
+//            if(n%2==1) return false;
+//            n /= 2;
+//        }
+//        return true;
+//    }
     // public static boolean isPowerofTwo(int n) {
     //     if(n==0) return false;
     //     if(n==1) return true;
