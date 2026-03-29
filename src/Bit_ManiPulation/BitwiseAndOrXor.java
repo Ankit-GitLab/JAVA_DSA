96433ae20617908f65762ebe5d0a95081078d050
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-03-29 11:13:40 +0530

    bitwise And Or

---
+++ src/Bit_ManiPulation/BitwiseAndOrXor.java
@@ -0,0 +1,8 @@
+package Bit_ManiPulation;
+
+public class BitwiseAndOrXor {
+    public static void main(String[] args) {
+        System.out.println(51&42);
+        System.out.println(51|42);
+    }
+}
