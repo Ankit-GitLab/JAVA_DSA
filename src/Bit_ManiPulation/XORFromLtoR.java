d4775e6a75ac527a756d374bf41f0a57fc3bc12c
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-03-30 19:32:24 +0530

    Xor from l to r

---
+++ src/Bit_ManiPulation/XORFromLtoR.java
@@ -0,0 +1,14 @@
+package Bit_ManiPulation;
+
+public class XORFromLtoR {
+    static int xor(int n) {
+        if(n%4==1) return 1;
+        else if(n%4==2) return n+1;
+        else if(n%4==3) return 0;
+        else  return n;
+
+    }
+    public static int findXOR(int l, int r) {
+        return xor(l-1)^xor(r);
+    }
+}
