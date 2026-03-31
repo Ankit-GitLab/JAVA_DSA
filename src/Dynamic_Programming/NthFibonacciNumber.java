c325a0f41080237e9ee929d70549e57f3048d842
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-03-31 14:58:40 +0530

    nth fibonacci number throught dp

---
+++ src/Dynamic_Programming/NthFibonacciNumber.java
@@ -0,0 +1,16 @@
+package Dynamic_Programming;
+
+public class NthFibonacciNumber {
+    static int[] dp;
+    public int fibo(int n) {
+        if(n<=1) return n;
+        if(dp[n]!=0) return dp[n];
+        int ans =  fibo(n-1) + fibo(n-2);
+        dp[n] = ans;
+        return ans;
+    }
+    public int fib(int n) {
+        dp = new int[n+1];
+        return fibo(n);
+    }
+}
