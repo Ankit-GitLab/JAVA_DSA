ecb8d5ffb5c6d296544adc57ed86c9928e5ff226
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-04-01 08:59:07 +0530

    nth fibonacci number throught tabulation

---
+++ src/Dynamic_Programming/Tabulation/NthFibonacciNumber.java
@@ -0,0 +1,13 @@
+package Dynamic_Programming.Tabulation;
+import java.util.*;
+public class NthFibonacciNumber {
+    public int nthFibonacci(int n) {
+        if(n<=1) return n;
+        int dp[] = new int[n+1];
+        dp[1] = 1;
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+}
