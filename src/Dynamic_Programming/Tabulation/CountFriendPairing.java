b40acac143760d3480ddec2c047f1816a6022286
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-04-03 09:40:59 +0530

    Tabulation

---
+++ src/Dynamic_Programming/Tabulation/CountFriendPairing.java
@@ -0,0 +1,15 @@
+package Dynamic_Programming.Tabulation;
+
+public class CountFriendPairing {
+    public long countFriendsPairings(int n) {
+        long[] dp = new long[n+1];
+        return friends(n,dp);
+    }
+
+    private long friends(int n, long[] dp) {
+        if(n<=2) return n;
+        if(dp[n]!=0) return dp[n];
+        return dp[n] = countFriendsPairings(n-1) + (n-1)*countFriendsPairings(n-2);
+
+    }
+}
