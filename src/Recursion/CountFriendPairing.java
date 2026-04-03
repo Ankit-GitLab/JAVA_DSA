764ca92a44e099b11a1234484214af17f16f8dcd
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-04-03 09:39:09 +0530

    Count friend pairing

--- src/Recursion/CountFriendPairing.java
+++ src/Recursion/CountFriendPairing.java
@@ -2,7 +2,7 @@ package Recursion;
 
 public class CountFriendPairing {
     public long countFriendsPairings(int n) {
-        if(n<=1) return n;
+        if(n<=2) return n;
         return countFriendsPairings(n-1) + (n-1)*countFriendsPairings(n-2);
     }
 }
