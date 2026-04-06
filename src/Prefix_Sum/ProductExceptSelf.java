4843d7b32decd6ada7b3da48c97e74dbd1711c69
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-04-06 13:15:17 +0530

    Reduce the code

--- src/Prefix_Sum/ProductExceptSelf.java
+++ src/Prefix_Sum/ProductExceptSelf.java
@@ -10,17 +10,61 @@ public class ProductExceptSelf {
             pre[i] = arr[i-1] * pre[i-1];
         }
 
-        int[] suf = new int[n];
-        suf[n-1] = 1;
+
+        int suf = 1;
         for(int i = n-2; i >= 0; i--){
-            suf[i] = arr[i+1] * suf[i+1];
+            suf *= arr[i+1];
+            pre[i] *= suf;
         }
 
-        int[] ans = new int[n];
-        for(int i = 0; i < n; i++){
-            ans[i] = pre[i] * suf[i];
-        }
-
-        return ans;
+        return pre;
     }
+
+
+//    public int[] productExceptSelf(int[] arr) {
+//        int n = arr.length;
+//
+//        int[] pre = new int[n];
+//        pre[0] = 1;
+//        for(int i = 1; i < n; i++){
+//            pre[i] = arr[i-1] * pre[i-1];
+//        }
+//
+//        int[] suf = new int[n];
+//        suf[n-1] = 1;
+//        for(int i = n-2; i >= 0; i--){
+//            suf[i] = arr[i+1] * suf[i+1];
+//        }
+//
+//
+//        for(int i = 0; i < n; i++){
+//            suf[i] *= pre[i];
+//        }
+//
+//        return suf;
+//    }
+
+
+//    public int[] productExceptSelf(int[] arr) {
+//        int n = arr.length;
+//
+//        int[] pre = new int[n];
+//        pre[0] = 1;
+//        for(int i = 1; i < n; i++){
+//            pre[i] = arr[i-1] * pre[i-1];
+//        }
+//
+//        int[] suf = new int[n];
+//        suf[n-1] = 1;
+//        for(int i = n-2; i >= 0; i--){
+//            suf[i] = arr[i+1] * suf[i+1];
+//        }
+//
+//        int[] ans = new int[n];
+//        for(int i = 0; i < n; i++){
+//            ans[i] = pre[i] * suf[i];
+//        }
+//
+//        return ans;
+//    }
 }
