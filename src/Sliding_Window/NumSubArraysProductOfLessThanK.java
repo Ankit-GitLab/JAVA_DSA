88183b30523e2878c018cdea4e2018d25c76d544
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-04-07 20:09:19 +0530

    number sub array product of less than k

--- src/Sliding_Window/NumSubArraysProductOfLessThanK.java
+++ src/Sliding_Window/NumSubArraysProductOfLessThanK.java
@@ -1,7 +1,24 @@
-//package Sliding_Window;
-//
-//public class NumSubArraysProductOfLessThanK {
-//    public int numSubarrayProductLessThanK(int[] nums, int k) {
-//return -1;
-//    }
-//}
+package Sliding_Window;
+
+public class NumSubArraysProductOfLessThanK {
+    public int numSubarrayProductLessThanK(int[] arr, int k) {
+        if (k <= 1) return 0;
+
+        int i = 0;
+        int prod = 1;
+        int count = 0;
+
+        for (int j = 0; j < arr.length; j++) {
+            prod *= arr[j];
+
+            while (prod >= k) {
+                prod /= arr[i];
+                i++;
+            }
+
+            count += (j - i + 1);
+        }
+
+        return count;
+    }
+}
