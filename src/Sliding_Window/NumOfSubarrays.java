ca2bd7d9d76b3ad0c86bc8b10d9da0bca37bf1ed
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-04-07 17:09:39 +0530

    Number of sub arrays

--- src/Sliding_Window/NumOfSubarrays.java
+++ src/Sliding_Window/NumOfSubarrays.java
@@ -1,7 +1,19 @@
-//package Sliding_Window;
-//
-//public class NumOfSubarrays {
-//    public int numOfSubarrays(int[] arr, int k, int threshold) {
-//
-//    }
-//}
+package Sliding_Window;
+
+public class NumOfSubarrays {
+    public int numOfSubarrays(int[] arr, int k, int t) {
+        int i=0, j=k-1, sum=0, count=0, n=arr.length;
+        for(int a=0; a<=k-1; a++){
+            sum += arr[a];
+        }
+
+        if(sum/k>=t) count++;
+        i++; j++;
+        while(j<n){
+            sum = sum - arr[i-1] + arr[j];
+            if(sum/k>=t) count++;
+            i++; j++;
+        }
+        return count;
+    }
+}
