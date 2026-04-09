bbd19826be4de905a64f4d0309e54bd314e043f0
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-04-09 23:28:55 +0530

    Majority element

---
+++ src/TCS/MajorityElement.java
@@ -0,0 +1,14 @@
+package TCS;
+import java.util.*;
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        int n = nums.length;
+
+        for(int ele : nums){
+            map.put(ele, map.getOrDefault(ele, 0) + 1);
+            if(map.get(ele) > n/2) return ele;
+        }
+        return -1;
+    }
+}
