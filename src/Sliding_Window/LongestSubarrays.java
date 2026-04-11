cb8362b7d8dcc1de39c82365fdd3edc687f537b2
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-04-11 23:43:30 +0530

    Longest sub Arrays

--- src/Sliding_Window/LongestSubarrays.java
+++ src/Sliding_Window/LongestSubarrays.java
@@ -1,4 +1,19 @@
 package Sliding_Window;
 
 public class LongestSubarrays {
+    public int longestSubarray(int[] arr) {
+        int i = 0, zeroes = 0, maxLen = 0;
+
+        for (int j = 0; j < arr.length; j++) {
+            if (arr[j] == 0) zeroes++;
+
+            while (zeroes > 1) {
+                if (arr[i] == 0) zeroes--;
+                i++;
+            }
+
+            maxLen = Math.max(maxLen, j - i);
+        }
+        return maxLen;
+    }
 }
