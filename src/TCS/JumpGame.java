98d0405832c285e1f00670e8dd97287e96bd30dc
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-04-14 21:13:25 +0530

    Jump game

---
+++ src/TCS/JumpGame.java
@@ -0,0 +1,15 @@
+package TCS;
+
+public class JumpGame {
+    public boolean canJump(int[] nums) {
+        int n =nums.length;
+        int maxReach = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (i > maxReach) return false;
+            maxReach = Math.max(maxReach, i + nums[i]);
+        }
+
+        return true;
+    }
+}
