3e00c92032de734b9523e1d61e636346d04b8ab7
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-04-17 22:42:05 +0530

    Contains duplicate

---
+++ src/LeetCode/ContainsDuplicate2.java
@@ -0,0 +1,19 @@
+package LeetCode;
+import java.util.*;
+public class ContainsDuplicate2 {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                int prevIndex = map.get(nums[i]);
+                if (i - prevIndex <= k) {
+                    return true;
+                }
+            }
+            map.put(nums[i], i);
+        }
+
+        return false;
+    }
+}
